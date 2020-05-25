cc28321c793f047486cebcc15c81597b864a04de
Sebastian Niecikowski <snitool@wp.pl>
2020-05-25 19:59:08 +0200

    git config, git template init

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ls -lA .dots/ | awk '{ print $NF }' | xargs -I{} ln -fs $(pwd)/.dots/{} /home/$USER/{} ;
+ln -fs $(pwd)/.git-template /home/$USER/.git-template ;
 
 dconf load /org/cinnamon/desktop/keybindings/ < dconf-settings.conf
 
