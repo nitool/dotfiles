0d3b0c32f24ab6d216ca6d5e7d8de52cd25c78d5
Sebastian Niecikowski <snitool@wp.pl>
2022-08-11 22:00:57 +0200

    reference processors

--- nvim-lua/statusline.lua
+++ nvim-lua/statusline.lua
@@ -1,12 +1,21 @@
 local ts_utils = require 'nvim-treesitter.ts_utils'
 
+local references_processors = {
+    php = require('references/php')
+}
+
 local function lsp_status()
+    local filetype = vim.api.nvim_buf_get_option(0, 'filetype');
+    if references_processors[filetype] == nil then
+        return nil
+    end
+
     local node = ts_utils.get_node_at_cursor();
     if node == nil then
         return '';
     end
 
-    return require('references/php').get_reference(node);
+    return references_processors[filetype].get_reference(node);
 end
 
 require('lualine').setup({
