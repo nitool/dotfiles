43fde7626245b017efc41ec3ab1d6744c8dfcbb9
Sebastian Niecikowski <snitool@wp.pl>
2024-01-08 15:41:43 +0100

    copilot

--- nvim-config/after/plugin/lsp.lua
+++ nvim-config/after/plugin/lsp.lua
@@ -84,3 +84,10 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true
 })
+
+vim.keymap.set('i', '<C-O>', 'copilot#Accept("<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+
+vim.g.copilot_no_tab_map = true
