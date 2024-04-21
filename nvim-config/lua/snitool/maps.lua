207216962da720a1b3770d3c0ca0d2e740284531
Sebastian Niecikowski <snitool@wp.pl>
2024-04-21 21:21:25 +0200

    quick fix list maps

--- nvim-config/lua/snitool/maps.lua
+++ nvim-config/lua/snitool/maps.lua
@@ -7,6 +7,8 @@ vim.keymap.set('n', '<leader>tn', ':tabnext<CR>', { noremap = true, silent = tru
 vim.keymap.set('n', '<leader>tp', ':tabprev<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>tt', ':tabnew<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<F1>', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>]', ':cnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>[', ':cprev<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('i', ',', ',<c-g>u', { noremap = true, silent = true })
 vim.keymap.set('i', '.', '.<c-g>u', { noremap = true, silent = true })
@@ -15,5 +17,4 @@ vim.keymap.set('i', '?', '?<c-g>u', { noremap = true, silent = true })
 
 vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
 vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
-
 
