dcdbacfb48d6821f45cdb60306c1fd4a9de13446
Sebastian Niecikowski <snitool@wp.pl>
2025-04-09 11:02:10 +0200

    fix harpoon

--- nvim-config/after/plugin/harpoon.lua
+++ nvim-config/after/plugin/harpoon.lua
@@ -11,28 +11,6 @@ vim.keymap.set("n", "<leader>mf", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>h]", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<leader>h[", function() harpoon:list():next() end)
 
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
+vim.keymap.set("n", "<leader>ll", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon window" })
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
-end
-
-vim.keymap.set("n", "<leader>ll", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
-
-vim.api.nvim_create_user_command('ClearHarpoon', function (opts)
-    harpoon:list():clear()
-end, { range = true })
 
